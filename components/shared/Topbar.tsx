c1af0f551ff4f54e9805aee786b912853950df6f
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2025-02-07 21:48:31 +0530

    bottombar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -6,24 +6,24 @@ function Topbar(){
     return (
         <nav className="topbar">
             <Link href="/" className="flex items-center gap-4">
-                <Image src="/assets/assets/logo.svg" alt="logo" width={28} height={28}/>
+                <Image src="/assets/logo.svg" alt="logo" width={28} height={28}/>
                 <p className="text-heading3-bold text-light-1 max-xs:hidden">Conexify</p>
             </Link>
 
             <div className="flex items-center gap-1">
                 <div className="block md:hidden">
-                    <SignedIn>  
+                     
                         <SignOutButton>
                             <div className="flex cursor-pointer">
                                 <Image 
-                                    src="/assets/assets/logout.svg"
+                                    src="/assets/logout.svg"
                                     alt="logout"
                                     width={24}
                                     height={24}
                                 /> 
                             </div>
                         </SignOutButton>
-                    </SignedIn>
+                    
                 </div>
 
                 <OrganizationSwitcher
