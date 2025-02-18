e786918e84a508cf672d27ff8438f0405426be94
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2025-02-19 02:11:49 +0530

    mongo backend

--- middleware.ts
+++ middleware.ts
@@ -21,6 +21,9 @@ import { clerkMiddleware, createRouteMatcher  } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/', '/api/webhook/clerk']);
 
+console.log("Middleware running");
+
+
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth.protect();
